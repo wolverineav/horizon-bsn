ad504d79bc14ae9a5a1e64abffa7e1d4c8f748eb
Aditya Vaja <wolverine.av@gmail.com>
2017-08-23 13:23:28 -0700

    IN-250: ensure build jobs don't pip/apt-get install

--- build/upload_to_pypi.sh
+++ build/upload_to_pypi.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -eux
-# install twine, to be added to infra puppet script
-sudo -H pip install twine
-CURR_VERSION=$(awk '/^version/{print $3}' setup.cfg)
 
-# get pypi and gpg creds in place
-mv $PYPIRC_FILE ~/.pypirc
-tar -zxvf $GNUPG_TAR -C ~/
+# RPM runs as root and doesn't like source files owned by a random UID
+OUTER_UID=$(stat -c '%u' /horizon-bsn)
+OUTER_GID=$(stat -c '%g' /horizon-bsn)
+trap "chown -R $OUTER_UID:$OUTER_GID /horizon-bsn" EXIT
+chown -R root:root /horizon-bsn
+
+cd /horizon-bsn
+git config --global user.name "Big Switch Networks"
+git config --global user.email "support@bigswitch.com"
+
+CURR_VERSION=$(awk '/^version/{print $3}' setup.cfg)
 
 echo 'CURR_VERSION=' $CURR_VERSION
 git tag -f -s $CURR_VERSION -m $CURR_VERSION -u "Big Switch Networks"
@@ -16,7 +21,7 @@ python setup.py sdist
 twine upload dist/* -r pypi -s -i "Big Switch Networks" || true
 # delay of 5 seconds
 sleep 5
-sudo -H pip install --no-cache-dir --upgrade horizon-bsn==$CURR_VERSION
+sudo -H pip install --upgrade horizon-bsn==$CURR_VERSION
 if [ "$?" -eq "0" ]
 then
   echo "PYPI upload successful."
@@ -26,6 +31,5 @@ fi
 # remove the package
 sudo -H pip uninstall -y horizon-bsn
 
-# remove pypi and gpg creds
-rm ~/.pypirc
-rm -rf ~/.gnupg
+# revert the permissions
+chown -R $OUTER_UID:$OUTER_GID /horizon-bsn
